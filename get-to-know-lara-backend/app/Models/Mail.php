1ab78717fc20ba8e061e1cd30c8e81cd58520cad
Anna Pomichal <rucsna@gmail.com>
2024-02-21 09:18:28 +0100

    feat: add fillables to Mail.php

--- get-to-know-lara-backend/app/Models/Mail.php
+++ get-to-know-lara-backend/app/Models/Mail.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Mail extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id_user_from',
+        'id_user_to',
+        'subject',
+        'message',
+        'is_read',
+        'sent'
+    ];
 }
