34fa917cc559b77f9c2379b5ffe92b6b0a7feeb3
Anna Pomichal <rucsna@gmail.com>
2024-02-26 21:59:03 +0100

    feat: add ContextProvider and BrowserRouter to react root

--- get-to-know-lara-frontend/src/main.jsx
+++ get-to-know-lara-frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from './Pages/Layout';
+import Router from './router';
+import { ContextProvider } from './contexts/ContextProvider';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <BrowserRouter>
+        <Layout />
+        <Router />
+      </BrowserRouter>
+    </ContextProvider>
   </React.StrictMode>,
 )
