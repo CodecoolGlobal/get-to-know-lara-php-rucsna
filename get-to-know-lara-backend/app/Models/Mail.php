574db1e1011fc2ff33b0b5ad1a2f126b577c4d15
Anna Pomichal <rucsna@gmail.com>
2024-02-26 21:44:40 +0100

    refactor: disable default timestamps from Mail.php model

--- get-to-know-lara-backend/app/Models/Mail.php
+++ get-to-know-lara-backend/app/Models/Mail.php
@@ -5,9 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static create(array $all)
+ * @method static findOrFail(string $id)
+ * @method static find(string $id)
+ */
 class Mail extends Model
 {
     use HasFactory;
+    public $timestamps = false;
     protected $fillable = [
         'id_user_from',
         'id_user_to',
