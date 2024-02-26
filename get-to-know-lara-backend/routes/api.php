96c6da3343979bf9f157556f59ff71539b580a0c
Anna Pomichal <rucsna@gmail.com>
2024-02-26 21:48:06 +0100

    feat: add routes to api.php

--- get-to-know-lara-backend/routes/api.php
+++ get-to-know-lara-backend/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\MailController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,10 +17,18 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('/mails', MailController::class);
 });
 
-Route::post("login", [UserController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::post("registration", [UserController::class, 'register']);
+//Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+
+
