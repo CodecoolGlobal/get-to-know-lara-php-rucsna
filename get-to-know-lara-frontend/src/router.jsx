8ac3d300c56fa40e4bbc14f62120c186c78074d6
Anna Pomichal <rucsna@gmail.com>
2024-04-29 09:37:10 +0200

    refactor: modify router.jsx to handle the two layouts

--- get-to-know-lara-frontend/src/router.jsx
+++ get-to-know-lara-frontend/src/router.jsx
@@ -1,22 +1,67 @@
-import { Route, Routes } from "react-router-dom";
-import { useStateContext } from "./contexts/ContextProvider";
-import Login from "./Pages/Login";
-import Registration from "./Pages/Registration";
+import {createBrowserRouter, Navigate} from "react-router-dom";
+import Login from "./Pages/GuestPages/Login.jsx";
+import Registration from "./Pages/GuestPages/Registration.jsx";
 import NotFound from "./Pages/NotFound";
-import Home from "./Pages/Home";
-import Profile from "./Pages/Profile";
+import UserLayout from "./Pages/Layouts/UserLayout.jsx";
+import GuestLayout from "./Pages/Layouts/GuestLayout.jsx";
+import Inbox from "./Pages/UserPages/Inbox.jsx";
+import Compose from "./Pages/UserPages/Compose.jsx";
+import Sent from "./Pages/UserPages/Sent.jsx";
+import CurrentMail from "./Pages/UserPages/CurrentMail.jsx";
+import About from "./Pages/GuestPages/About.jsx";
 
-const Router = () => {
-    const {token} = useStateContext();
-
-    return(
-            <Routes>
-                <Route path="/" element={token ? <Profile/> : <Home/>} />
-                <Route path="/registration" element={<Registration/>} />
-                <Route path="/login" element={<Login/>} />
-                <Route path="*" element={<NotFound/>} />
-            </Routes>
-    );
-};
-
-export default Router;
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <UserLayout />,
+        children: [
+            {
+              path: '/',
+              element: <Navigate to='/inbox' />
+            },
+            {
+                path: '/inbox',
+                element: <Inbox />
+            },
+            {
+                path: '/compose',
+                element: <Compose />
+            },
+            {
+                path: '/sent',
+                element: <Sent />
+            },
+            {
+                path: '/currentMail/:id',
+                element: <CurrentMail />
+            }
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Login />
+            },
+            {
+              path: '/about',
+              element: <About />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/registration',
+                element: <Registration />
+            }
+        ]
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    }
+]);
+export default router;
