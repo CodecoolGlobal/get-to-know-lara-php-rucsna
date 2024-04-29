2a23c54d00df2be9e28640f50008f3d54ec9479a
Anna Pomichal <rucsna@gmail.com>
2024-04-29 09:43:08 +0200

    refactor: change imported files' path according to new directory structure

--- get-to-know-lara-frontend/src/Pages/GuestPages/Registration.jsx
+++ get-to-know-lara-frontend/src/Pages/GuestPages/Registration.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axiosClient from "../axios-client";
-import { useStateContext } from "../contexts/ContextProvider";
+import axiosClient from "../../axios-client.js";
+import { useStateContext } from "../../contexts/ContextProvider.jsx";
 
 const Registration = () => {
     const [currentUsername, setCurrentUsername] = useState("");
