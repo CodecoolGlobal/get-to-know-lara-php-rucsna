12827c251227304f575e1e28ba25d7e5c62a5ab9
Anna Pomichal <rucsna@gmail.com>
2024-04-29 09:32:21 +0200

    feat: create TransactionSeeder.php to add transaction data to database

---
+++ get-to-know-lara-backend/database/seeders/TransactionSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TransactionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('transactions')->insert([
+            ['user_id' => 2, 'mail_id' => 1, 'sent_at' => now(), 'received_at' => null],
+            ['user_id' => 1, 'mail_id' => 1, 'sent_at' => null, 'received_at' => now()],
+            ['user_id' => 1, 'mail_id' => 2, 'sent_at' => now(), 'received_at' => null],
+            ['user_id' => 3, 'mail_id' => 2, 'sent_at' => null, 'received_at' => now()],
+            ['user_id' => 1, 'mail_id' => 3, 'sent_at' => now(), 'received_at' => null],
+            ['user_id' => 2, 'mail_id' => 3, 'sent_at' => null, 'received_at' => now()],
+            ['user_id' => 3, 'mail_id' => 4, 'sent_at' => now(), 'received_at' => null],
+            ['user_id' => 1, 'mail_id' => 4, 'sent_at' => null, 'received_at' => now()],
+            ['user_id' => 2, 'mail_id' => 5, 'sent_at' => now(), 'received_at' => null],
+            ['user_id' => 1, 'mail_id' => 5, 'sent_at' => null, 'received_at' => now()]
+        ]);
+    }
+}
