49c27654f5cfee8b8ca7e7e806b3732add451534
Anna Pomichal <rucsna@gmail.com>
2024-04-29 09:18:39 +0200

    feat: add mails() function to User model

--- get-to-know-lara-backend/app/Models/User.php
+++ get-to-know-lara-backend/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +43,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function mails(): BelongsToMany
+    {
+        return $this
+            ->belongsToMany(Mail::class, 'transactions', 'user_id', 'mail_id')
+            ->withPivot('received_at', 'sent_at')
+            ->whereNull('deleted_at');
+    }
+
 }
