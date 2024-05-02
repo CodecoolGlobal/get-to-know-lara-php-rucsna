8c956177026c3c3d614d709a3183e3aa59f26996
Anna Pomichal <rucsna@gmail.com>
2024-05-02 11:09:06 +0200

    refactor: add json responses to controller functions

--- get-to-know-lara-backend/app/Http/Controllers/MailController.php
+++ get-to-know-lara-backend/app/Http/Controllers/MailController.php
@@ -7,9 +7,12 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\Mail;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Validator;
 
 class MailController extends Controller
 {
@@ -19,49 +22,83 @@ class MailController extends Controller
      * Display a listing of the mails data the user received/sent (latest first).
      * Only those that are not deleted.
      */
-    public function getMailsByUser(string $type, string $userId): \Illuminate\Support\Collection
+    public function getMailsByUser(string $type, string $userId): JsonResponse
     {
-        $user = User::query()->findOrFail($userId);
+        try {
+            $user = User::query()->findOrFail($userId);
+            if (!$user) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'User not found'
+                ], Response::HTTP_NOT_FOUND);
+            }
 
-        $mails = $user->mails()
-            ->with(['user_from', 'user_to'])
-            ->whereNull('deleted_at')
-            ->whereNotNull($type === 'inbox' ? 'received_at' : 'sent_at')
-            ->orderByDesc($type === 'inbox' ? 'received_at' : 'sent_at')
-            ->get();
+            $mails = $user->mails()
+                ->with(['user_from', 'user_to'])
+                ->whereNull('deleted_at')
+                ->whereNotNull($type === 'inbox' ? 'received_at' : 'sent_at')
+                ->orderByDesc($type === 'inbox' ? 'received_at' : 'sent_at')
+                ->get();
 
-        return $mails->map(function ($mail) use ($type) {
-            $user = $type === 'inbox' ? 'user_from' : 'user_to';
-            return [
-                $user . '_name' => $mail->{$user}->name,
-                $user . '_email' => $mail->{$user}->email,
-                'subject' => $mail->subject,
-                'time' => $mail->pivot->{$type === 'inbox' ? 'received_at' : 'sent_at'},
-                'opened_at' => $mail->pivot->opened_at
-            ];
-        });
+            $mailsResponse = $mails->map(function ($mail) use ($type) {
+                $user = $type === 'inbox' ? 'user_from' : 'user_to';
+                return [
+                    $user . '_name' => $mail->{$user}->name,
+                    $user . '_email' => $mail->{$user}->email,
+                    'subject' => $mail->subject,
+                    'time' => $mail->pivot->{$type === 'inbox' ? 'received_at' : 'sent_at'},
+                    'opened_at' => $mail->pivot->opened_at
+                ];
+            });
+            return response()->json([
+                'success' => true,
+                'mails' => $mailsResponse
+            ], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            return \response()->json([
+                'success' => false,
+                'message' => 'Error fetching mails' . $exception->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
      * Store a newly created mail as a draft.
-     * The mail is not sent yet, receiver, subject, message, attachment, reply all optional.
+     * The mail is not sent yet, receiver, subject, message, attachment, reply_to all optional fields.
      */
-    public function createDraft(Request $request): Model|Builder
+    public function createDraft(Request $request): JsonResponse
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'from' => 'required|exists:users,id',
             'to' => 'sometimes|nullable|exists:users,id',
             'reply_to' => 'sometimes|nullable|exists:mails,id'
         ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation error'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
 
-        return Mail::query()->create([
-            'user_id_from' => $request['from'],
-            'user_id_to' => $request['to'],
-            'subject' => $request['subject'],
-            'message' => $request['message'],
-            'attachment' => $request['attachment'],
-            'reply_to' => $request['reply_to']
-        ]);
+        try {
+            $newDraft = Mail::query()->create([
+                'user_id_from' => $request['from'],
+                'user_id_to' => $request['to'],
+                'subject' => $request['subject'],
+                'message' => $request['message'],
+                'attachment' => $request['attachment'],
+                'reply_to' => $request['reply_to']
+            ]);
+            return response()->json([
+                'success' => true,
+                'draft' => $newDraft
+            ], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error creating draft' . $exception->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
@@ -70,35 +107,55 @@ class MailController extends Controller
      */
     public function sendMail(Request $request, ?string $mailId = null)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'from' => 'required|exists:users,id',
             'to' => 'required|exists:users,id',
             'reply_to' => 'sometimes|nullable|exists:mails,id'
         ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation error'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
         if ($mailId === null) {
-            return DB::transaction(function () use ($request){
-                $newMail = Mail::query()->create([
+            try {
+                DB::transaction(function () use ($request) {
+                    $newMail = Mail::create([
                         'user_id_from' => $request['from'],
                         'user_id_to' => $request['to'],
                         'subject' => $request['subject'],
                         'message' => $request['message']
-                ]);
+                    ]);
 
-                Transaction::query()->create([
+                    $transactionFrom = Transaction::query()->create([
                         'user_id' => $request['from'],
                         'mail_id' => $newMail->id,
                         'sent_at' => now()
-                ]);
+                    ]);
 
-                Transaction::query()->create([
+                    $transactionTo = Transaction::query()->create([
                         'user_id' => $request['to'],
                         'mail_id' => $newMail->id,
                         'received_at' => now()
-                ]);
-                return $newMail;
+                    ]);
+                    return response()->json([
+                        'success' => true,
+                        'mail' => $newMail,
+                        'sent' => $transactionFrom,
+                        'received' => $transactionTo
+                    ], Response::HTTP_CREATED);
                 });
+            } catch (\Exception $exception) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Error creating new mail or transactions' . $exception->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+            }
         } else {
-               return DB::transaction(function () use ($request, $mailId){
+            try {
+                DB::transaction(function () use ($request, $mailId) {
                     $updatedMail = Mail::query()->find($mailId);
                     $updatedMail->update([
                         'user_id_to' => $request['to'],
@@ -107,18 +164,29 @@ class MailController extends Controller
                         'attachment' => $request['attachment']
                     ]);
                     $updatedMail->save();
-                    Transaction::query()->create([
+                    $transactionFrom = Transaction::query()->create([
                         'user_id' => $request['from'],
                         'mail_id' => $mailId,
                         'sent_at' => now()
                     ]);
-                    Transaction::query()->create([
+                    $transactionTo = Transaction::query()->create([
                         'user_id' => $request['to'],
                         'mail_id' => $mailId,
                         'received_at' => now()
                     ]);
-                    return $updatedMail;
+                    return response()->json([
+                        'success' => true,
+                        'mail' => $updatedMail,
+                        'sent' => $transactionFrom,
+                        'received' => $transactionTo
+                    ], Response::HTTP_OK);
                 });
+            } catch (\Exception $exception) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Error updating mail or transactions' . $exception->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+            }
         }
     }
 
@@ -126,68 +194,209 @@ class MailController extends Controller
      * Display the specified mail.
      * Update the related transaction with opened_at timestamp.
      */
-    public function readMail(string $mailId): Builder|array|Collection|Model
+    public function displayMail(string $mailId): JsonResponse
     {
-        $mailToOpen = Mail::query()->findOrFail($mailId);
-        $mailToOpen->transactions()
-            ->whereNotNull('received_at')
-            ->whereNull('deleted_at')
-            ->update(['opened_at' => now()]);
-        return $mailToOpen;
+        $mailToDisplay = Mail::query()->find($mailId);
+        if (!$mailToDisplay) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Mail not found'
+            ], Response::HTTP_NOT_FOUND);
+        } else {
+            try {
+                $mailToDisplay->transactions()
+                    ->whereNotNull('received_at')
+                    ->whereNull('deleted_at')
+                    ->whereNull('opened_at')
+                    ->update(['opened_at' => now()]);
+                return response()->json([
+                    'success' => true,
+                    'mail' => $mailToDisplay
+                ], Response::HTTP_OK);
+            } catch (\Exception $exception) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Error displaying mail ' . $exception->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Soft delete the specified mail.
      * Update the related transaction with deleted_at timestamp.
      */
-    public function remove(string $mailId, string $userId): Model|Collection|Builder|array|null
+    public function remove(string $mailId, string $userId): JsonResponse
     {
-        $mailToDelete = Mail::query()->findOrFail($mailId);
-        $mailToDelete->transactions()
-            ->where('user_id', $userId)
-            ->whereNull('deleted_at')
-            ->update(['deleted_at' => now()]);
-        return $mailToDelete;
+        $user = User::query()->find($userId);
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $mailToDelete = $user->mails->where('id', $mailId)->first();
+        if (!$mailToDelete) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Mail not found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        if ($mailToDelete->transactions()->where('user_id', $userId)->whereNotNull('deleted_at')->exists()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Mail is already deleted'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try {
+            $mailToDelete->transactions()
+                ->where('user_id', $userId)
+                ->whereNull('deleted_at')
+                ->update(['deleted_at' => now()]);
+            $mailToDelete->refresh();
+
+            return response()->json([
+                'success' => true,
+                'mail' => $mailToDelete
+            ], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error removing mail ' . $exception->getMessage(),
+                'mail' => $mailToDelete
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
-     *
+     * Display the user's deleted mails.
+     * List data through transactions table 'deleted_at' field.
      */
-    public function getDeletedMails(string $userId)
+    public function getDeletedMails(string $userId): JsonResponse
     {
-        $user = User::query()->findOrFail($userId);
+        $user = User::find($userId);
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found'
+            ],Response::HTTP_NOT_FOUND);
+        }
 
-        $deletedMails = $user->mails()->whereNotNull('deleted_at')->orderByDesc('deleted_at')->get();
+        try {
+            $deletedMails = $user->mails()->whereNotNull('deleted_at')->orderByDesc('deleted_at')->get();
 
-        return $deletedMails->map(function ($mail) use ($userId){
-            $user = $mail->user_id_from === $userId ? 'user_from' : 'user_to';
-            return [
-                $user . '_name' => $mail->{$user}->name,
-                $user . '_email' => $mail->{$user}->email,
-                'subject' => $mail->subject,
-                'time' => $mail->pivot->deleted_at
-            ];
-        });
+            $mails = $deletedMails->map(function ($mail) use ($userId) {
+                $user = $mail->user_id_from === $userId ? 'user_from' : 'user_to';
+                return [
+                    $user . '_name' => $mail->{$user}->name,
+                    $user . '_email' => $mail->{$user}->email,
+                    'subject' => $mail->subject,
+                    'time' => $mail->pivot->deleted_at
+                ];
+            });
+            return response()->json([
+                'success' => true,
+                'mails' => $mails
+            ], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error fetching mails ' . $exception->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 
-    public function getDraftsByUser(string $userId)
+    /**
+     * Restore the user's deleted mails.
+     * Set the 'deleted_at' field back to null.
+     */
+    public function restoreDeletedMail(string $mailId, string $userId): JsonResponse
     {
-        // get all mail by user where userId == user_id_from
-        // omit the mails that has transactions
-        // return those
+        $user = User::find($userId);
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found'
+            ],Response::HTTP_NOT_FOUND);
+        }
+
+        $mailToRestore = $user->mails->where('id', $mailId)->first();
+        if (!$mailToRestore) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Mail not found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        if ($mailToRestore->transactions()->where('user_id', $userId)->whereNull('deleted_at')->exists()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Mail is not deleted'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try {
+            $mailToRestore->transactions()
+                ->where('user_id', $userId)
+                ->whereNotNull('deleted_at')
+                ->update(['deleted_at' => null]);
+            $mailToRestore->refresh();
+
+            return \response()->json([
+                'success' => true,
+                'mail' => $mailToRestore
+            ], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error restoring mail ' . $exception->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
     }
 
-//
+    /**
+     * Remove the specified mail from storage.
+     */
+    public function deleteDraft($id): JsonResponse
+    {
+        // check if exists
+        // check if draft - with no transaction
+        try {
+            $deletedDraftId = Mail::destroy($id);
+            return response()->json([
+                'success' => true,
+                'mailId' => $deletedDraftId
+            ], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error deleting draft ' . $exception->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+    }
 
-
-//    public function getTransactionByUser(string $id){
-//        $user = User::query()->find($id);
-//        return $user->received()->get();
-//    }
-//
-//    public function getUser(string $id)
-//    {
-//        $mail = Mail::query()->find($id);
-//        return $mail->user_to()->get();
-//    }
+    public function getDraftsByUser(string $userId): JsonResponse
+    {
+        $drafts = collect();
+        try {
+            $mails = Mail::query()->where('user_id_from', $userId)->get();
+            $mails->each(function ($mail) use ($drafts) {
+                if ($mail->transactions()->count() === 0) {
+                    $drafts->push($mail);
+                }
+            });
+            return response()->json([
+                'success' => true,
+                'drafts' => $drafts
+            ], Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error fetching drafts ' . $exception->getMessage()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
