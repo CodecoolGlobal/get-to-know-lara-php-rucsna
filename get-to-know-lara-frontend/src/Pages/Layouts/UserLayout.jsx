d16e12217ccdf6e39b7cff7293209f34a7075df5
Anna Pomichal <rucsna@gmail.com>
2024-05-27 13:42:28 +0200

    refactor: add Bootstrap to layouts

--- get-to-know-lara-frontend/src/Pages/Layouts/UserLayout.jsx
+++ get-to-know-lara-frontend/src/Pages/Layouts/UserLayout.jsx
@@ -1,35 +1,38 @@
-import {Navigate, Outlet, useNavigate} from "react-router-dom";
+import {Link, Outlet, useLocation, useNavigate} from "react-router-dom";
 import {useStateContext} from "../../contexts/ContextProvider.jsx";
 import axiosClient from "../../axios-client.js";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 
 function UserLayout() {
-    const [isInbox, setIsInbox] = useState(true);
-    const [isCompose, setIsCompose] = useState(false);
-    const [isSent, setIsSent] = useState(false);
-    const [isBin, setIsBin] = useState(false);
-
     const {token, user, setUser, storeToken} = useStateContext();
     const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         const getUser = () => {
             axiosClient.get('/user')
                 .then(response => {
                     setUser(response.data);
-                    console.log(response.data);
+                    //console.log(response.data);
                 })
                 .catch(err => {
                     console.error("Error with getting user data", err);
                 })
         };
-        if(token){
+        if (!token) {
+            navigate('/login');
+        } else if (token && !user) {
             getUser();
+            console.log(location.pathname);
         }
-    }, [setUser, token]);
+    }, [location.pathname, navigate, setUser, token, user]);
+
+    if (!user) {
+        return <div>Loading...</div>
+    }
 
     const handleLogout = () => {
-        axiosClient.post('/logout')
+        axiosClient.post('/authentication/logout')
             .then(() => {
                 setUser({});
                 storeToken(null);
@@ -40,51 +43,23 @@ function UserLayout() {
             })
     };
 
-    const handleNavigation = (path) => {
-        setIsInbox(path === '/inbox');
-        setIsCompose(path === '/compose');
-        setIsSent(path === '/sent');
-        setIsBin(path === '/bin');
-        navigate(path);
-    }
-
-    if (!token) {
-        return <Navigate to={'/login'}/>
-    }
-
     return (
         <div className="container">
-            <nav className="navbar navbar-expand-sm bg-light justify-content-end">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/get-to-know-lara-frontend/public">{user.name}</a>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link" onClick={handleLogout}>Logout</button>
-                    </li>
-                </ul>
+            <nav className="navbar navbar-expand-sm bg-dark-subtle">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to={"/"}>ZM</Link>
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active text-success-emphasis" to={"/dashboard"}>{user.name}</Link>
+                        </li>
+                        <li className="nav-item">
+                            <button className="nav-link active text-success-emphasis" onClick={handleLogout}>Logout
+                            </button>
+                        </li>
+                    </ul>
+                </div>
             </nav>
-                <div className="row">
-                    <div className="col-md-3">
-                        <div className="btn-group-vertical" role="group" aria-label="Basic example">
-                            <button type="button" className={`btn ${isCompose ? 'btn-secondary' : 'btn'}`}
-                                    onClick={() => handleNavigation('/compose')}>Compose
-                            </button>
-                            <button type="button" className={`btn ${isInbox ? 'btn-secondary' : 'btn'}`}
-                                    onClick={() => handleNavigation('/inbox')}>Inbox
-                            </button>
-                            <button type="button" className={`btn ${isSent ? 'btn-secondary' : 'btn'}`}
-                                    onClick={() => handleNavigation('/sent')}>Sent
-                            </button>
-                            <button type="button" className={`btn ${isBin ? 'btn-secondary' : 'btn'}`}
-                                    onClick={() => handleNavigation('/bin')}>Bin
-                            </button>
-                        </div>
-                    </div>
-                <div className="col-md-6 offset md-3">
-                    <Outlet/>
-                </div>
-                </div>
+            <Outlet/>
         </div>
     )
 }
