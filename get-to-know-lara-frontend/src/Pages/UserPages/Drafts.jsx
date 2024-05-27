c56170a3fadc699d757e36c173cb01e184b75a31
Anna Pomichal <rucsna@gmail.com>
2024-05-27 13:34:53 +0200

    feat: create Drafts.jsx page

---
+++ get-to-know-lara-frontend/src/Pages/UserPages/Drafts.jsx
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react";
+import axiosClient from "../../axios-client.js";
+import {useStateContext} from "../../contexts/ContextProvider.jsx";
+import MailsTable from "../../Components/MailsTable.jsx";
+
+const Drafts = () => {
+    const [mails, setMails] = useState();
+    const {user} = useStateContext();
+
+    useEffect(() => {
+        getDrafts();
+    }, [user.id]);
+
+    const getDrafts = () => {
+        axiosClient.get(`/mail/mailsByUser/draft/${user.id}`)
+            .then(response => {
+                setMails(response.data.mails);
+                console.log(response.data.mails);
+            })
+            .catch (error => console.error("Error fetching mails", error))
+    };
+
+    return(
+        <div>
+            {mails &&
+                <MailsTable mails={mails} setMails={setMails} getCurrent={getDrafts} isDraft={true} label={"Draft "}/>
+            }
+        </div>
+    );
+}
+
+export default Drafts;
