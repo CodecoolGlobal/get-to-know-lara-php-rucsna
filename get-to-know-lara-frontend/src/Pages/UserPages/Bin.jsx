9e50404bd4b679a80a48c1ebf09ba0756abaf659
Anna Pomichal <rucsna@gmail.com>
2024-05-27 13:28:43 +0200

    feat: create Bin page

---
+++ get-to-know-lara-frontend/src/Pages/UserPages/Bin.jsx
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react";
+import axiosClient from "../../axios-client.js";
+import {useStateContext} from "../../contexts/ContextProvider.jsx";
+import MailsTable from "../../Components/MailsTable.jsx";
+
+const Bin = () => {
+    const [mails, setMails] = useState();
+    const {user} = useStateContext();
+
+    useEffect(() => {
+        getDeletedMails();
+    }, [user.id]);
+
+    const getDeletedMails = () => {
+        axiosClient.get(`/mail/mailsByUser/deleted/${user.id}`)
+            .then(response => {
+                setMails(response.data.mails);
+                console.log(response.data.mails);
+            })
+            .catch (error => console.error("Error fetching mails", error))
+    };
+
+    return(
+        <div>
+            {mails &&
+               <MailsTable mails={mails} setMails={setMails} getCurrent={getDeletedMails} isBin={true} label={"Deleted "}/>
+            }
+        </div>
+    );
+}
+
+export default Bin;
