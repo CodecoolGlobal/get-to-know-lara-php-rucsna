6c3d1a5e4f49f14cec518031e7e852d1cad8283c
Anna Pomichal <rucsna@gmail.com>
2024-05-28 10:36:07 +0200

    chore: setup vitest testing environment

---
+++ get-to-know-lara-frontend/tests/unit-tests/setupTests.js
@@ -0,0 +1,25 @@
+import {expect} from "vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
+expect.extend(matchers);
+//
+// const mails = [
+//     {
+//         'id': 1,
+//         'name': 'Jake Peralta',
+//         'subject': 'test subject',
+//         'message': 'test message'
+//     },
+//     {
+//         'id': 2,
+//         'name': 'Amy Santiago',
+//         'subject': 'grammar',
+//         'message': 'a message about grammar'
+//     },
+//     {
+//         'id': 3,
+//         'name': 'Charles Boyle',
+//         'subject': 'food',
+//         'message': 'what you eat matters'
+//     }
+// ];
+
