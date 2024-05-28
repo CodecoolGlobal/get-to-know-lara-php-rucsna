d7deb1d0b341f5c051b24e6930f5147ce92bc507
Anna Pomichal <rucsna@gmail.com>
2024-05-28 23:32:34 +0200

    feat: add invalid feedbacks to Login.jsx page

--- get-to-know-lara-frontend/src/Pages/GuestPages/Login.jsx
+++ get-to-know-lara-frontend/src/Pages/GuestPages/Login.jsx
@@ -1,32 +1,57 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useStateContext } from "../../contexts/ContextProvider.jsx";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {useStateContext} from "../../contexts/ContextProvider.jsx";
 import axiosClient from "../../axios-client.js";
 
 const Login = () => {
-    const [currentEmail, setCurrentEmail] = useState("");
-    const [currentPassword, setCurrentPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailValid, setEmailValid] = useState(true);
+    const [passwordValid, setPasswordValid] = useState(true);
 
     const navigate = useNavigate();
-    const { setUser, storeToken } = useStateContext();
+    const {setUser, storeToken} = useStateContext();
+
+    const validateInputs = () => {
+        let valid = true;
+        if (!emailValid) {
+            setEmailValid(false);
+            valid = false;
+        } else {
+            setEmailValid(true);
+        }
+
+        if (!passwordValid) {
+            setPasswordValid(false);
+            valid = false;
+        } else {
+            setPasswordValid(false);
+        }
+        return valid;
+    }
 
     const submitLogin = async (event) => {
         event.preventDefault();
 
+        if (!validateInputs()) {
+            return;
+        }
+        ;
+
         const currentUser = {
-            email: currentEmail,
-            password: currentPassword
+            email: email,
+            password: password
         };
 
         try {
-            const { data } = await axiosClient.post('/authentication/login', currentUser);
+            const {data} = await axiosClient.post('/authentication/login', currentUser);
             console.log(data);
             setUser(data.user);
             storeToken(data.token);
             navigate('/');
 
         } catch (err) {
-           console.error("error logging in", err);
+            console.error("error logging in", err);
         }
     };
 
@@ -34,37 +59,47 @@ const Login = () => {
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-6 col-lg-4">
-            <fieldset>
-                <legend className="text-center">Have an account? Please, log in!</legend>
-                <form className="mx-auto" id="login-form" onSubmit={submitLogin}>
+                    <fieldset>
+                        <legend className="text-center fs-5">Have an account? Please, log in!</legend>
+                        <form className="mx-auto" id="login-form" onSubmit={submitLogin}>
 
-                    <div className="mb-3 mt-5">
-                        <label htmlFor="inputEmail" className="form-label">Email address</label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            id="inputEmail"
-                            aria-describedby="emailHelp"
-                            onChange={e => setCurrentEmail(e.target.value)}/>
-                    </div>
+                            <div className="mb-3 mt-5">
+                                <label htmlFor="inputEmail" className="form-label">Email address</label>
+                                <input
+                                    type="email"
+                                    className={`form-control ${emailValid ? '' : 'is-invalid'} `}
+                                    id="inputEmail"
+                                    aria-describedby="emailHelp"
+                                    onChange={e => setEmail(e.target.value)}
+                                    required
+                                />
+                                <div className="invalid-feedback">
+                                    Please enter your email address.
+                                </div>
+                            </div>
 
-                    <div className="mb-3">
-                        <label htmlFor="inputPassword" className="form-label">Password</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            id="inputPassword"
-                            onChange={e => setCurrentPassword(e.target.value)}/>
-                        <div id="emailHelp" className="form-text">Forget password?</div>
-                    </div>
+                            <div className="mb-3">
+                                <label htmlFor="inputPassword" className="form-label">Password</label>
+                                <input
+                                    type="password"
+                                    className={`form-control ${passwordValid ? '' : 'is-invalid'}`}
+                                    id="inputPassword"
+                                    onChange={e => setPassword(e.target.value)}
+                                    required
+                                />
+                                <div className="invalid-feedback">
+                                    Please enter your password.
+                                </div>
+                                <div id="emailHelp" className="form-text">Forget password?</div>
+                            </div>
 
-                    <button type="submit" className="btn btn-primary">Login</button>
-                </form>
-            </fieldset>
+                            <button type="submit" className="btn btn-primary">Login</button>
+                        </form>
+                    </fieldset>
                 </div>
             </div>
         </div>
-)
+    )
 }
 
 export default Login;
