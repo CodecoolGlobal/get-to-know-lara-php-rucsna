c1704a4804016d3cf0d8e2c1b034450e3fc120a7
Anna Pomichal <rucsna@gmail.com>
2024-05-29 18:29:13 +0200

    refactor: change GuestLayout.jsx with react-bootstrap components

--- get-to-know-lara-frontend/src/Pages/Layouts/GuestLayout.jsx
+++ get-to-know-lara-frontend/src/Pages/Layouts/GuestLayout.jsx
@@ -1,39 +1,33 @@
 import {Navigate, Outlet} from "react-router-dom";
 import {useStateContext} from "../../contexts/ContextProvider.jsx";
 
+import {Container, Nav, Navbar, Image, NavbarBrand} from "react-bootstrap";
+
 function GuestLayout() {
     const {token} = useStateContext();
-    if(token){
-        return <Navigate to={'/'} />
+    if (token) {
+        return <Navigate to={'/'}/>
     }
 
     return (
-        <div className="container">
-            <nav className="navbar navbar-expand-sm bg-dark-subtle">
-                <div className="container-fluid">
-                    <a className="navbar-brand text-uppercase" href={"/"}>ZM</a>
-
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href={"/login"}>Login</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active" href={"/registration"}>Sign up</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+        <div>
+            <Navbar expand="sm" className="bg-dark-subtle">
+                <Container className="container-fluid">
+                    <NavbarBrand href="/">
+                        <Image src="../../../assets/ZM_logo.png" style={{maxHeight: '60px'}}/>
+                    </NavbarBrand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/login">Login</Nav.Link>
+                            <Nav.Link href="/registration">Sign up</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <Outlet/>
         </div>
-    )
+    );
 }
 
 export default GuestLayout;
