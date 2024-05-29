6b3245408716ab2aadfde9aa198baeea82704026
Anna Pomichal <rucsna@gmail.com>
2024-05-29 18:30:04 +0200

    refactor: change Login.jsx with react-bootstrap components

--- get-to-know-lara-frontend/src/Pages/GuestPages/Login.jsx
+++ get-to-know-lara-frontend/src/Pages/GuestPages/Login.jsx
@@ -4,107 +4,114 @@ import {useStateContext} from "../../contexts/ContextProvider.jsx";
 import axiosClient from "../../axios-client.js";
 import ErrorToastMessage from "../../Components/ErrorToastMessage.jsx";
 
+import {Button, Form} from "react-bootstrap";
+import FormContainer from "../../Components/FormContainer.jsx";
+
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [emailValid, setEmailValid] = useState(true);
-    const [passwordValid, setPasswordValid] = useState(true);
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    });
+    const [errors, setErrors] = useState({});
     const [loginSuccess, setLoginSuccess] = useState(true);
 
     const navigate = useNavigate();
     const {setUser, storeToken} = useStateContext();
 
-    const validateInputs = () => {
-        let valid = true;
-        if (!emailValid) {
-            setEmailValid(false);
-            valid = false;
-        } else {
-            setEmailValid(true);
-        }
+    const setField = (field, value) => {
+        setForm({
+            ...form,
+            [field]: value
+        });
 
-        if (!passwordValid) {
-            setPasswordValid(false);
-            valid = false;
-        } else {
-            setPasswordValid(false);
+        if(!errors[field]) {
+            setErrors({
+                ...errors,
+                [field]: null
+            });
         }
-        return valid;
+    }
+
+    const validateForm = () => {
+        const {email, password} = form;
+        const newErrors = {};
+
+        if(!email){
+            newErrors.email = "Please, enter your email address.";
+        }
+        if(!password){
+            newErrors.password = "Please, enter your password.";
+        }
+        return newErrors;
     }
 
     const submitLogin = async (event) => {
         event.preventDefault();
 
-        if (!validateInputs()) {
+        const formErrors = validateForm();
+        if(Object.keys(formErrors).length > 0){
+            setErrors(formErrors);
             return;
         }
-        ;
 
         const currentUser = {
-            email: email,
-            password: password
+            email: form.email,
+            password: form.password
         };
 
         try {
             const {data} = await axiosClient.post('/authentication/login', currentUser);
-            console.log(data);
             setUser(data.user);
             storeToken(data.token);
             navigate('/');
 
         } catch (err) {
             setLoginSuccess(false);
-            console.error("error logging in", err);
         }
     };
 
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-6 col-lg-4">
-                    <fieldset>
-                        <legend className="text-center fs-5">Have an account? Please, log in!</legend>
-                        <form className="mx-auto" id="login-form" onSubmit={submitLogin}>
+            <FormContainer>
+                <fieldset>
+                <legend className="text-sm-center fs-5 fst-italic">Have an account? Please, log in!</legend>
+                    {!loginSuccess &&
+                        <ErrorToastMessage toastHeader={"Login failed"} toastMessage={"Username or password is invalid"}/>
+                    }
+                <Form className="mt-5" onSubmit={submitLogin}>
+                    <Form.Group controlId="email">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            className="mb-2"
+                            type="email"
+                            value={form.email}
+                            onChange={(e) => setField('email', e.target.value)}
+                            isInvalid={!!errors.email}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.email}
+                        </Form.Control.Feedback>
+                    </Form.Group>
 
-                            <div className="mb-3 mt-5">
-                                <label htmlFor="inputEmail" className="form-label">Email address</label>
-                                <input
-                                    type="email"
-                                    className={`form-control ${emailValid ? '' : 'is-invalid'} `}
-                                    id="inputEmail"
-                                    aria-describedby="emailHelp"
-                                    onChange={e => setEmail(e.target.value)}
-                                    required
-                                />
-                                <div className="invalid-feedback">
-                                    Please enter your email address.
-                                </div>
-                            </div>
+                    <Form.Group controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            className="mb-2"
+                            type="password"
+                            value={form.password}
+                            onChange={(e) => setField('password', e.target.value)}
+                            isInvalid={!!errors.password}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.password}
+                        </Form.Control.Feedback>
+                    </Form.Group>
 
-                            <div className="mb-3">
-                                <label htmlFor="inputPassword" className="form-label">Password</label>
-                                <input
-                                    type="password"
-                                    className={`form-control ${passwordValid ? '' : 'is-invalid'}`}
-                                    id="inputPassword"
-                                    onChange={e => setPassword(e.target.value)}
-                                    required
-                                />
-                                <div className="invalid-feedback">
-                                    Please enter your password.
-                                </div>
-                                <div id="emailHelp" className="form-text">Forget password?</div>
-                            </div>
-
-                            <button type="submit" className="btn btn-primary">Login</button>
-                        </form>
-                    </fieldset>
-                </div>
-                {!loginSuccess &&
-                    <ErrorToastMessage toastHeader={"Login failed"} toastMessage={"Username or password is invalid"}/>
-                }
-            </div>
-        </div>
+                    <Button variant="secondary" type="submit">Login</Button>
+                </Form>
+                </fieldset>
+            </FormContainer>
     )
 }
 
