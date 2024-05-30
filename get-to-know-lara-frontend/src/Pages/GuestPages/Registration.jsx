508db71275278cff58d94df02fe161ea0cc11a74
Anna Pomichal <rucsna@gmail.com>
2024-05-30 09:37:56 +0200

    fix: correct password confirmation

--- get-to-know-lara-frontend/src/Pages/GuestPages/Registration.jsx
+++ get-to-know-lara-frontend/src/Pages/GuestPages/Registration.jsx
@@ -47,21 +47,29 @@ const Registration = () => {
         }
     }
     const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-        console.log("PASS", password);
-        if(password && password.length < 7){
-            setPasswordError("Password should be min 6 characters long");
+        const currentPassword = event.target.value;
+        setPassword(currentPassword);
+
+        console.log("PASS", currentPassword);
+        if(password && password.length < 6){
+            setPasswordError("Password should be at least 6 characters long");
         } else if(!password.match(/.*[0-9].*/)){
             setPasswordError("Password should contain a number");
         } else {
             setPasswordError("");
         }
+        if(passwordConf && passwordConf !== currentPassword){
+            setPasswordError("Password and Password confirm should match");
+        }
     }
 
     const handlePasswordConfirmChange = (event) => {
-        setPasswordConf(event.target.value);
-        console.log("PASS", passwordConf);
-        if(passwordConf && passwordConf !== password){
+        const currentPasswordConf = event.target.value;
+        setPasswordConf(currentPasswordConf);
+
+        console.log("curr-PASS", currentPasswordConf);
+        console.log("password-", password);
+        if(currentPasswordConf !== password){
             setPasswordConfError("Password and Password confirm should match");
         } else {
             setPasswordConfError("");
@@ -96,31 +104,33 @@ const Registration = () => {
         <FormContainer>
             <fieldset>
             <legend className="text-sm-center fs-5 fst-italic">New on the page? Please, sign up!</legend>
-            <Form className="mt-5" onSubmit={submitRegistration}>
+            <Form className="mt-5" onSubmit={submitRegistration} noValidate>
                 <Row>
                     <Form.Group as={Col} controlId="firstName">
                         <Form.Label>Name</Form.Label>
                         <Form.Control
-                            className={`mb-2 ${passwordError ? "border-danger" : ""}`}
+                            className={`mb-2 ${firstNameError ? "border-danger" : ""}`}
                             type="text"
                             placeholder="First name"
                             required
                             title={firstNameError}
                             onChange={handleFirstNameChange}
                         />
-                        <p className="text-danger fs-6 h4">{firstNameError}</p>
+                        {firstNameError &&
+                        <p className="text-danger fs-6 fst-italic">{firstNameError}</p>
+                        }
                     </Form.Group>
                     <Form.Group as={Col} controlId="lastName">
                         <Form.Label>Name</Form.Label>
                         <Form.Control
-                            className={`mb-2 ${passwordError ? "border-danger" : ""}`}
+                            className={`mb-2 ${lastNameError ? "border-danger" : ""}`}
                             type="text"
                             placeholder="Last name"
                             required
                             title={lastNameError}
                             onChange={handleLastNameChange}
                         />
-                        <p className="text-danger fs-6 h4">{lastNameError}</p>
+                        <p className="text-danger fs-6 fst-italic">{lastNameError}</p>
                     </Form.Group>
                 </Row>
 
@@ -135,7 +145,7 @@ const Registration = () => {
                         onChange={handleEmailChange}
                         value={email}
                     />
-                    <p className="text-danger fs-6 h4">{emailError}</p>
+                    <p className="text-danger fs-6 fst-italic">{emailError}</p>
                 </Form.Group>
 
                 <Form.Group controlId="password">
@@ -150,7 +160,7 @@ const Registration = () => {
                         onChange={handlePasswordChange}
                         value={password}
                     />
-                    <p className="text-danger fs-6 h4">{passwordError}</p>
+                    <p className="text-danger fs-6 fst-italic">{passwordError}</p>
                 </Form.Group>
 
                 <Form.Group controlId="confirmPassword">
@@ -163,7 +173,7 @@ const Registration = () => {
                         onChange={handlePasswordConfirmChange}
                         value={passwordConf}
                     />
-                    <p className="text-danger fs-6 h4">{passwordConfError}</p>
+                    <p className="text-danger fs-6 fst-italic">{passwordConfError}</p>
                 </Form.Group>
                 <Button variant="secondary" type="submit">Sign up</Button>
             </Form>
