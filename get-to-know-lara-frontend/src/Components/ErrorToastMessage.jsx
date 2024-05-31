6019ee7e5c8cd6bd2c79bf8be23ab5df34c508fc
Anna Pomichal <rucsna@gmail.com>
2024-05-31 20:29:55 +0200

    refactor: add react-bootstrap components to ErrorToastMessage.jsx

--- get-to-know-lara-frontend/src/Components/ErrorToastMessage.jsx
+++ get-to-know-lara-frontend/src/Components/ErrorToastMessage.jsx
@@ -1,32 +1,26 @@
 import PropTypes from "prop-types";
-import {useEffect} from "react";
+//import {useEffect} from "react";
+import {Toast} from "react-bootstrap";
+import {useState} from "react";
 
 const ErrorToastMessage = ({toastHeader, toastMessage}) => {
-    ErrorToastMessage.propTypes = {
-        toastHeader: PropTypes.string,
-        toastMessage: PropTypes.string
-    };
-
-    useEffect(() => {
-        const toastContent = document.querySelector('.toast');
-
-        const toast = new bootstrap.Toast(toastContent);
-        toast.show();
-    }, []);
+    const [show, setShow] = useState(true);
 
     return(
-        <div aria-live="polite" aria-atomic="true" className="d-flex justify-content-center align-items-center w-100">
-            <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                <div className="toast-header">
-                    <strong className="me-auto">{toastHeader}</strong>
-                    <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                </div>
-                <div className="toast-body">
-                    {toastMessage}
-                </div>
-            </div>
-        </div>
+        <Toast className="border-danger" onClose={() => setShow(false)} show={show}>
+            <Toast.Header>
+                <strong className="me-auto text-danger">{toastHeader}</strong>
+            </Toast.Header>
+            <Toast.Body className="text-danger">
+                {toastMessage}
+            </Toast.Body>
+        </Toast>
     );
-}
+};
+
+ErrorToastMessage.propTypes = {
+    toastHeader: PropTypes.string,
+    toastMessage: PropTypes.string
+};
 
 export default ErrorToastMessage;
