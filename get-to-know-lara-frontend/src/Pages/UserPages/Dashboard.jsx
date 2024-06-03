d54f8a5bf2184ac5279c9b2d0219c6a154e2fbca
Anna Pomichal <rucsna@gmail.com>
2024-06-03 13:49:51 +0200

    refactor: add bootstrap icons to buttons

--- get-to-know-lara-frontend/src/Pages/UserPages/Dashboard.jsx
+++ get-to-know-lara-frontend/src/Pages/UserPages/Dashboard.jsx
@@ -11,23 +11,23 @@ const Dashboard = () => {
             <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                 <button className="nav-link active" id="v-pills-inbox-tab" data-bs-toggle="pill"
                         data-bs-target="#v-pills-inbox" type="button" role="tab" aria-controls="v-pills-inbox"
-                        aria-selected="true">Inbox
+                        aria-selected="true"><i className="bi bi-mailbox"></i> Inbox
                 </button>
                 <button className="nav-link" id="v-pills-compose-tab" data-bs-toggle="pill"
                         data-bs-target="#v-pills-compose" type="button" role="tab" aria-controls="v-pills-compose"
-                        aria-selected="false">Compose
+                        aria-selected="false"><i className="bi bi-pencil-square"></i> Compose
                 </button>
                 <button className="nav-link" id="v-pills-drafts-tab" data-bs-toggle="pill"
                         data-bs-target="#v-pills-drafts" type="button" role="tab" aria-controls="v-pills-drafts"
-                        aria-selected="false">Drafts
+                        aria-selected="false"><i className="bi bi-file-text"></i> Drafts
                 </button>
                 <button className="nav-link" id="v-pills-sent-tab" data-bs-toggle="pill"
                         data-bs-target="#v-pills-sent" type="button" role="tab" aria-controls="v-pills-sent"
-                        aria-selected="false">Sent
+                        aria-selected="false"><i className="bi bi-send-check"></i> Sent
                 </button>
                 <button className="nav-link" id="v-pills-bin-tab" data-bs-toggle="pill"
                         data-bs-target="#v-pills-bin" type="button" role="tab" aria-controls="v-pills-bin"
-                        aria-selected="false">Bin
+                        aria-selected="false"><i className="bi bi-trash3"></i> Bin
                 </button>
             </div>
             <div className="tab-content" id="v-pills-tabContent">
