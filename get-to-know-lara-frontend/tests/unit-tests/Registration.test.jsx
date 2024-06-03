a807f7ab5548c986f8decfeacd40311d25a8436a
Anna Pomichal <rucsna@gmail.com>
2024-06-03 13:51:02 +0200

    refactor: add MemoryRouter to Registration.test.jsx

--- get-to-know-lara-frontend/tests/unit-tests/Registration.test.jsx
+++ get-to-know-lara-frontend/tests/unit-tests/Registration.test.jsx
@@ -1,18 +1,20 @@
 import {describe, test, expect, beforeEach} from "vitest";
-import {fireEvent, render, screen} from "@testing-library/react";
+import {fireEvent, render, screen, waitFor} from "@testing-library/react";
 import Registration from "../../src/Pages/GuestPages/Registration.jsx";
-import {BrowserRouter} from "react-router-dom";
+import {MemoryRouter, Route, Routes} from "react-router-dom";
 
 describe('Register component', () => {
     beforeEach(() => {
         render(
-            <BrowserRouter>
-                <Registration/>
-            </BrowserRouter>);
+            <MemoryRouter initialEntries={["/guest/registration"]}>
+                <Routes>
+                    <Route path="/guest/registration" element={<Registration/>} />
+                </Routes>
+            </MemoryRouter>);
     });
 
-
     test('renders form fields and submit button', () => {
+        // assert
         expect(screen.getByLabelText('Name', {exact: false})).toBeInTheDocument();
         expect(screen.getByPlaceholderText('First name', {exact: false})).toBeInTheDocument();
         expect(screen.getByPlaceholderText('Last name', {exact: false})).toBeInTheDocument();
@@ -23,30 +25,33 @@ describe('Register component', () => {
     });
 
     test('updates form fields correctly', () => {
+        // arrange
         const firstNameInput = screen.getByPlaceholderText('First name', {exact: false});
-        fireEvent.change(firstNameInput, {target: {value: 'Amy'}});
-        expect(firstNameInput.value).toBe('Amy');
-
         const lastNameInput = screen.getByPlaceholderText('Last name', {exact: false});
-        fireEvent.change(lastNameInput, {target: {value: 'Santiago'}});
-        expect(lastNameInput.value).toBe('Santiago');
-
         const emailInput = screen.getByLabelText('Email address', {exact: false});
-        fireEvent.change(emailInput, {target: {value: 'santiago@nypd.com'}});
-        expect(emailInput.value).toBe('santiago@nypd.com');
-
         const passwordInput = screen.getByLabelText('Password', {exact: true});
-        fireEvent.change(passwordInput, {target: {value: 'password123'}});
-        expect(passwordInput.value).toBe('password123');
-
         const passConfInput = screen.getByLabelText('Confirm password', {exact: false});
+
+        // act
+        fireEvent.change(firstNameInput, {target: {value: 'Amy'}});
+        fireEvent.change(lastNameInput, {target: {value: 'Santiago'}});
+        fireEvent.change(emailInput, {target: {value: 'santiago@nypd.com'}});
+        fireEvent.change(passwordInput, {target: {value: 'password123'}});
         fireEvent.change(passConfInput, {target: {value: 'password123'}});
+
+        // assert
+        expect(firstNameInput.value).toBe('Amy');
+        expect(lastNameInput.value).toBe('Santiago');
+        expect(emailInput.value).toBe('santiago@nypd.com');
+        expect(passwordInput.value).toBe('password123');
         expect(passConfInput.value).toBe('password123');
     });
 
     test('shows validation messages for empty fields', () => {
+        // act
         fireEvent.submit(screen.getByRole('button', {name: 'Sign up'}));
 
+        // assert
         expect(screen.getByText('Please, enter your first name', {exact: false})).toBeInTheDocument();
         expect(screen.getByText('Please, enter your family name', {exact: false})).toBeInTheDocument();
         expect(screen.getByText('Please, enter a valid email address', {exact: false})).toBeInTheDocument();
@@ -55,26 +60,70 @@ describe('Register component', () => {
     });
 
     test('shows validation message for short password', () => {
+        // arrange
         const passwordInput = screen.getByLabelText('Password', {exact: true});
+
+        // act
         fireEvent.change(passwordInput, {target: {value: '123'}});
         fireEvent.submit(screen.getByRole('button', {name: 'Sign up'}));
+
+        // assert
         expect(screen.getByText('Password should be at least 6 characters long', {exact: false})).toBeInTheDocument();
     });
 
     test('shows validation message for password without number', () => {
+        // arrange
         const passwordInput = screen.getByLabelText('Password', {exact: true});
+
+        // act
         fireEvent.change(passwordInput, {target: {value: 'password'}});
         fireEvent.submit(screen.getByRole('button', {name: 'Sign up'}));
+
+        // assert
         expect(screen.getByText('Password should contain a number', {exact: false})).toBeInTheDocument();
     });
 
     test('shows validation message for not matching passwords', () => {
+        //arrange
         const passwordInput = screen.getByLabelText('Password', {exact: true});
         const passConfInput = screen.getByLabelText('Confirm password', {exact: false});
 
+        // act
         fireEvent.change(passwordInput, {target: {value: 'password123'}});
         fireEvent.change(passConfInput, {target: {value: '123password'}});
         fireEvent.submit(screen.getByRole('button', {name: 'Sign up'}));
+
+        // assert
         expect((screen.getAllByText('Passwords do not match', {exact: false})).length).toBeGreaterThan(0);
     });
+
+    test('calls submitRegistration on form submission', async () => {
+        // arrange
+        const firstNameInput = screen.getByPlaceholderText('First name', {exact: false});
+        const lastNameInput = screen.getByPlaceholderText('Last name', {exact: false});
+        const emailInput = screen.getByLabelText('Email address', {exact: false});
+        const passwordInput = screen.getByLabelText('Password', {exact: true});
+        const passConfInput = screen.getByLabelText('Confirm password', {exact: false});
+
+        // act
+        fireEvent.change(firstNameInput, {target: {value: 'Amy'}});
+        fireEvent.change(lastNameInput, {target: {value: 'Santiago'}});
+        fireEvent.change(emailInput, {target: {value: 'santiago@nypd.com'}});
+        fireEvent.change(passwordInput, {target: {value: 'password123'}});
+        fireEvent.change(passConfInput, {target: {value: 'password123'}});
+
+        fireEvent.submit(screen.getByRole('button', {name: 'Sign up'}));
+
+        // assert
+        await waitFor(() => {
+            expect(screen.queryByText('Please, enter your first name', {exact: false})).not.toBeInTheDocument();
+            expect(screen.queryByText('Please, enter a valid email address', {exact: false})).not.toBeInTheDocument();
+            expect(screen.queryByText('Passwords do not match', {exact: false})).not.toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            expect(window.location.pathname).toBe('/');
+        });
+
+    })
 })
