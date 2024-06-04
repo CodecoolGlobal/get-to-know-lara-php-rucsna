c571723834b46133ff007b4c1a31a968ba3c80b0
Anna Pomichal <rucsna@gmail.com>
2024-06-04 22:58:57 +0200

    refactor: modify MailForm.jsx to decrease redundancy

--- get-to-know-lara-frontend/src/Components/MailForm.jsx
+++ get-to-know-lara-frontend/src/Components/MailForm.jsx
@@ -1,70 +1,67 @@
-import {useEffect, useState} from "react";import {useStateContext} from "../contexts/ContextProvider.jsx";
+import {useCallback, useEffect, useState} from "react";import {useStateContext} from "../contexts/ContextProvider.jsx";
 import PropTypes from "prop-types";
 
 import SearchBar from "./SearchBar.jsx";
 import FormContainer from "./FormContainer.jsx";
 import {Button, Card, FloatingLabel, Form, FormControl, InputGroup} from "react-bootstrap";
+import AttachmentUpload from "./AttachmentUpload.jsx";
 
 const MailForm = ({mail, onSend, onSave}) => {
-    const [userIdTo, setUserIdTo] = useState("");
-    const [email, setEmail] = useState("");
-    const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState( "");
+    const [form, setForm] = useState({
+        user_id_to: "",
+        email: "",
+        subject: "",
+        message: "",
+        attachment: null
+    });
+    const [errors, setErrors] = useState({});
+
     const {user} = useStateContext();
 
     useEffect(() => {
         if(mail){
-            setUserIdTo(mail.user_id_to);
-            setEmail(mail.email);
-            setSubject(mail.subject);
-            setMessage(mail.message);
+            setForm({...mail});
         }
     }, [mail]);
+
+    const handleFieldChange = useCallback((field, value) => {
+        setForm(prevForm => ({...prevForm, [field]: value}));
+        if(errors[field]){
+            setErrors(prevErrors => ({...prevErrors, [field]: null}));
+        }
+    },[errors]);
+
+
+    const handleSave = () => {
+        if(mail){
+            return onSave({...mail, ...form, is_draft: false});
+        } else{
+            return onSave({user_id_from: user.id, ...form, is_draft: false});
+        }
+    };
+
+    const handleSend = () => {
+        if(mail){
+            return onSend({...mail, ...form, is_draft: false});
+        } else {
+            return onSend({user_id_from: user.id, ...form, is_draft: false});
+        }
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const {name} = event.nativeEvent.submitter;
 
         if(name === "saveButton"){
-            if(mail){
-                return onSave({
-                    ...mail,
-                    //user_id_from: user.id,
-                    user_id_to: userIdTo,
-                    subject,
-                    message,
-                    is_draft: false
-                });
-            }
-            return onSave({
-                user_id_from: user.id,
-                user_id_to: userIdTo,
-                subject: subject,
-                message: message,
-                is_draft: false
-            });
+            handleSave();
         }
 
         if(name === "sendButton"){
-            if(mail){
-                return onSend({
-                    ...mail,
-                    //user_id_from: user.id,
-                    user_id_to: userIdTo,
-                    subject,
-                    message,
-                    is_draft: false
-                });
-            }
-            return onSend({
-                user_id_from: user.id,
-                user_id_to: userIdTo,
-                subject: subject,
-                message: message,
-                is_draft: false
-            });
+            handleSend();
         }
-    }
+    };
+
+    console.log('FORM', form);
 
     return (
         <FormContainer>
@@ -72,7 +69,7 @@ const MailForm = ({mail, onSend, onSave}) => {
                 <Card.Header className="text-light">New message</Card.Header>
                 <Card.Body>
                     <Form className="row g-3" onSubmit={handleSubmit}>
-                        <SearchBar setUser={setUserIdTo} email={email}/>
+                        <SearchBar setUser={handleFieldChange} email={form.email}/>
 
                         <InputGroup className="col-md-6">
                             <InputGroup.Text className="text-light" id="subject">Subject:</InputGroup.Text>
@@ -81,8 +78,8 @@ const MailForm = ({mail, onSend, onSave}) => {
                                 aria-label="subject"
                                 aria-describedby="subject"
                                 placeholder="example subject"
-                                value={subject}
-                                onChange={(e) => setSubject(e.target.value)}/>
+                                value={form.subject}
+                                onChange={e => handleFieldChange('subject', e.target.value)}/>
                         </InputGroup>
 
                         <InputGroup className="col-md-12 has-validation">
@@ -91,16 +88,13 @@ const MailForm = ({mail, onSend, onSave}) => {
                                 as="textarea"
                                 style={{height: '200px'}}
                                 placeholder="Type your message here..."
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
+                                value={form.message}
+                                onChange={e => handleFieldChange('message', e.target.value)}
                                 required/>
                             </FloatingLabel>
                         </InputGroup>
 
-                        <Form.Group controlId="file-input" className="mb-3">
-                            <Form.Label className="text-light">Attachment</Form.Label>
-                            <Form.Control type="file" multiple />
-                        </Form.Group>
+                        <AttachmentUpload handleFieldChange={handleFieldChange}/>
 
                         <InputGroup>
                             <Button type="submit" name="sendButton" className="btn-success btn-outline-light">Send</Button>
