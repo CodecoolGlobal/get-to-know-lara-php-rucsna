bf13819217c494372b6219873cc7840b913683ed
Anna Pomichal <rucsna@gmail.com>
2024-06-06 12:56:28 +0200

    feat: create Attachment model

---
+++ get-to-know-lara-backend/app/Models/Attachment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Attachment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'mail_id',
+        'filename',
+        'file',
+    ];
+
+    public function mail(): BelongsTo
+    {
+        return $this-> belongsTo(Mail::class);
+    }
+}
