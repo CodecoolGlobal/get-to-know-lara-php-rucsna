e3290fbc7250e55c27e6de7d4979ee74fbf68f75
Anna Pomichal <rucsna@gmail.com>
2024-06-06 12:55:42 +0200

    refactor: modify mails table to simplify the database schema

--- get-to-know-lara-backend/database/migrations/2024_02_19_212840_create_mails_table.php
+++ get-to-know-lara-backend/database/migrations/2024_02_19_212840_create_mails_table.php
@@ -13,12 +13,9 @@ return new class extends Migration
     {
         Schema::create('mails', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id_from')->constrained('users')->onDelete('restrict');
-            $table->foreignId('user_id_to')->nullable()->constrained('users')->onDelete('restrict');
             $table->string('subject')->nullable();
             $table->text('message')->nullable();
             $table->boolean('is_draft')->default(true);
-            $table->binary('attachment')->nullable();
             $table->foreignId('reply_to')->nullable()->constrained('mails')->onDelete('set null');
             $table->timestamp('created_at')->useCurrent();
         });
