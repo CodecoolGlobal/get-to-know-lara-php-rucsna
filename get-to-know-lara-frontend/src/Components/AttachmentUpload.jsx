4a147a70504ff337c17b1c779eaf4e06dac68e67
Anna Pomichal <rucsna@gmail.com>
2024-06-06 13:06:41 +0200

    refactor: clean up AttachmentUpload.jsx

--- get-to-know-lara-frontend/src/Components/AttachmentUpload.jsx
+++ get-to-know-lara-frontend/src/Components/AttachmentUpload.jsx
@@ -1,41 +1,22 @@
 import {Form} from "react-bootstrap";
-import {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 
 const AttachmentUpload = ({handleFieldChange}) => {
-    const [files, setFiles] = useState([]);
-
-    const packFiles = (files) => {
-        const data = new FormData();
-        [...files].forEach((file, i) => {
-            data.append(`file-${i}`, file, file.name);
-        });
-        console.log('packed files', data);
-        return data;
-    };
-
-    useEffect(() => {
-        if(files.length){
-            console.log('files', files);
-            handleFieldChange('files', packFiles(files));
-        }
-    }, [files, handleFieldChange]);
-
     return(
         <Form.Group controlId="file-input" className="mb-3">
             <Form.Label className="text-light">Attachment</Form.Label>
             <Form.Control
                 type="file"
-                multiple
                 accept="image/*, .pdf, audio/*, .doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-                onChange={e => setFiles(e.target.files)}
+                onChange={e => handleFieldChange('attachment', e.target.files)}
+                multiple
             />
         </Form.Group>
     )
 };
 
 AttachmentUpload.propTypes = {
-    handleFieldChange: PropTypes.func
+    handleFieldChange: PropTypes.func.isRequired
 };
 
 export default AttachmentUpload;
